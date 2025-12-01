07825c3f6bbb127b049cff9f809061b94406498c
Camiel Jalink <35895867+CamielJalink@users.noreply.github.com>
2025-12-01 15:00:58 +0100

    Day1Part1 solution

--- src/day1/part1/index.ts
+++ src/day1/part1/index.ts
@@ -1,7 +1,30 @@
 import { readFileSync } from "fs";
 
 export default function advent() {
-    const stringInput = readFileSync("input/day1-test.txt", "utf-8");
-    const input = stringInput.split(/\n\n/gm);
-    console.log(input);
+    const stringInput = readFileSync("input/day1.txt", "utf-8");
+    const input = stringInput.split(/\r\n/gm);
+    console.log(findPassword(input));
+}
+
+function findPassword(input: string[]) {
+    let password = 0;
+    let currentPosition = 50;
+
+    for( let i = 0; i < input.length; i++ ) {
+        let direction = input[i][0];
+        let amount = parseInt(input[i].slice(1));
+
+        if(direction === "L") {
+            currentPosition -= amount;
+        } else if(direction === "R") {
+            currentPosition += amount;
+        }
+
+        currentPosition = currentPosition % 100;
+
+        if(currentPosition === 0) {
+            password++;
+        }
+    }
+    return password;
 }
