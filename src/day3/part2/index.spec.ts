744186d9b1f535473f1743893b164aa90d2686e3
Camiel <camiel.jalink@gmail.com>
2025-12-05 15:11:00 +0100

    Copied part1 and setup new unit tests for part 2

--- src/day3/part2/index.spec.ts
+++ src/day3/part2/index.spec.ts
@@ -1,6 +1,14 @@
 import UnitTest from "../../helpers/unittest";
+import { findJoltage } from ".";
 
 // testcases
 export default function runTests() {
-    console.log("Hello unit tests!");
+    const findJoltageTester = new UnitTest<string, number>((batteryArray: string) => findJoltage(batteryArray));
+
+    findJoltageTester.runTests([
+        { input: "987654321111111", expected: 987654321111 },
+        { input: "811111111111119", expected: 811111111119 },
+        { input: "234234234234278", expected: 434234234278 },
+        { input: "818181911112111", expected: 888911112111 },
+    ]);
 }
--- src/day3/part2/index.ts
+++ src/day3/part2/index.ts
@@ -3,7 +3,40 @@ import runTests from "./index.spec";
 
 export function advent() {
     const stringInput = readFileSync("input/day3-test.txt", "utf-8");
-    const input = stringInput.split(/\n\n/gm);
+    const input = stringInput.split(/\r\n/gm);
     runTests();
-    console.log(input);
+    console.log(findTotalJoltage(input));
+}
+
+function findTotalJoltage(batteryArrays: string[]) {
+    let totalJoltage = 0;
+
+    batteryArrays.forEach((batteryArray: string) => {
+        totalJoltage += findJoltage(batteryArray);
+    });
+
+    return totalJoltage;
+}
+
+export function findJoltage(batteryArray: string) {
+    const firstBattery: { joltage: string; index: number } = { joltage: "0", index: 0 };
+
+    // The last character in batteryArray may not be the first digit.
+    for (let i = 0; i < batteryArray.length - 1; i++) {
+        if (batteryArray[i] > firstBattery.joltage) {
+            firstBattery.joltage = batteryArray[i];
+            firstBattery.index = i;
+        }
+    }
+
+    // Don't check the battery's before the first battery again.
+    const secondBattery: { joltage: string; index: number } = { joltage: "0", index: 0 };
+    for (let i = firstBattery.index + 1; i < batteryArray.length; i++) {
+        if (batteryArray[i] > secondBattery.joltage) {
+            secondBattery.joltage = batteryArray[i];
+            secondBattery.index = i;
+        }
+    }
+
+    return parseInt(firstBattery.joltage + secondBattery.joltage);
 }
